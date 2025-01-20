7e6b7d511032e3aa622889f2eb5233b224578bd2
Maxelone <maxelone@yandex.ru>
2025-01-20 09:22:40 +0400

    Nvim: Add opts for autosave: debounce_delay 8s and debug false

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -4,6 +4,8 @@ return {
   cmd = "ASToggle", -- optional for lazy loading on command
   event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
   opts = {
+    debounce_delay = 8000,
+    debug = false,
     -- your config goes here
     -- or just leave it empty :)
   },
