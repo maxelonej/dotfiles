29fda1dceace9135181bdf22900e047342951339
Maxelone <maxelone@yandex.ru>
2025-01-21 19:01:55 +0400

    Update treesitter, remove version to not get an exit bug when editing html

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,45 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		tag = "v0.9.1",
-		opts = {
-			ensure_installed = {
-				"javascript",
-				"typescript",
-				"css",
-				"gitignore",
-				"graphql",
-				"http",
-				"json",
-				"scss",
-				"sql",
-				"vim",
-				"lua",
-			},
-			query_linter = {
-				enable = true,
-				use_virtual_text = true,
-				lint_events = { "BufWrite", "CursorHold" },
-			},
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    branch = "master",
+    opts = {
+      ensure_installed = {
+        "javascript",
+        "typescript",
+        "css",
+        "gitignore",
+        "graphql",
+        "http",
+        "json",
+        "scss",
+        "sql",
+        "vim",
+        "lua",
+      },
+      query_linter = {
+        enable = true,
+        use_virtual_text = true,
+        lint_events = { "BufWrite", "CursorHold" },
+      },
+      highlight = {
+        cenable = true,
+        disable = function(lang, buf)
+          if lang == "html" then
+            return true
+          end
+          local max_filesize = 100 * 1024
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            vim.notify(
+              "File larger than 100KB treesitter disabled for performance",
+              vim.log.levels.WARN,
+              { title = "treesitter" }
+            )
+            return true
+          end
+        end,
+      },
+    },
+  },
 }
