f388bd01675f8afefdfa378c814460e1ae633bb5
Maxelone <maxelone@yandex.ru>
2025-02-05 01:40:19 +0400

    Update lualine, remove default lazyvim cfg to custom one

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,47 @@
 return {
+
+  -- Lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function()
+      local opts = {
+        options = {
+          theme = "auto",
+          -- section_separators = { left = "", right = "" },
+          -- section_separators = " ",
+          component_separators = "",
+          always_show_tabline = true,
+        },
+        sections = {
+          lualine_a = {
+            {
+              "mode",
+              icons_enabled = true,
+            },
+          },
+          lualine_b = { "filename", "branch" },
+          -- lualine_c = { "fileformat" },
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = { "filetype", "progress" },
+          lualine_z = { "location" },
+        },
+        inactive_sessions = {
+          lualine_a = { "filename" },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { "location" },
+        },
+        tabline = {},
+        extensions = {},
+      }
+
+      return opts
+    end,
+  },
+
   {
     "akinsho/bufferline.nvim",
     keys = {
