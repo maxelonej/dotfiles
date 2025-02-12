62021b7a99fa1200e1d3db318c27c499d2520a05
Maxelone <maxelone@yandex.ru>
2025-02-12 08:18:18 +0400

    Delete colorschemes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,6 @@
 return {
   {
     -- "rebelot/kanagawa.nvim",
-    --   "sainnhe/sonokai",
-    --   priority = 1000,
-    --   config = function()
-    --     vim.g.sonokai_transparent_background = "1"
-    --     vim.g.sonokai_enable_italic = "1"
-    --     vim.g.sonokai_style = "andromeda"
-    --     vim.cmd.colorscheme("sonokai")
-    --   end,
+    -- "sainnhe/sonokai",
   },
 }
